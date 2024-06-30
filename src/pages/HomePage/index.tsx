ea0b93b32eec1cc4c23b0d0eb2a2fc9057df3a68
Raissa Toledo <raissalst@gmail.com>
2024-06-30 17:05:14 -0300

    feat: create folders and main component draft

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,37 @@
+/* import { Card } from "../../components/Card";
+import { NavBar } from "../../components/NavBar";
+import { ListContainerHome, MainContainerHome } from "./style";
+import { MenuItemFormat } from "../../interfaces/interfaces";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useCart } from "../../providers/cart";
+import { useAuth } from "../../providers/auth"; */
+
+const HomePage = () => {
+  /*  const [menuList, setMenuList] = useState<MenuItemFormat[]>(
+    [] as MenuItemFormat[]
+  );
+
+  const { getItemsInCart, setCart } = useCart();
+
+  const { authToken, userId } = useAuth();
+
+  useEffect(() => {
+    axios
+      .get("https://hamburgerapi.onrender.com/menulist")
+      .then((response) => {
+        setMenuList([...response.data]);
+        userId && authToken && getItemsInCart(userId, authToken);
+        userId === "" && setCart([]);
+      })
+      .catch((e) => console.log(e.response.data.message));
+  }, [userId]); */
+
+  return (
+    <div>
+      <div>My Profile</div>
+    </div>
+  );
+};
+
+export default HomePage;
