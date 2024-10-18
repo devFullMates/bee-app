5d8fb91beec68e62ddb26aca24ab978400ce5c8c
larissakoliveira <oliveir5uwm@gmail.com>
2024-10-18 19:40:06 -0300

    fix: remove react imports not being used

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import ErrorBoundary from './components/ErrorBoundary';
 import Header from './components/Header';
 const RoutesPath = lazy(() => import('./routes'));
